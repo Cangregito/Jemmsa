b1351ad3de48c98c41a1ef08d8438e59ff076128
Langostin <erick.2468.mx@gmail.com>
2026-02-03 17:16:13 -0700

    Carrusel inicial

---
+++ public/js/carrusel_data.js
@@ -0,0 +1,90 @@
+    const carouselData = [
+    {
+        title: 'Directivos',
+        description:
+        'Sillas de alta gama diseñadas para directivos, con materiales premium, ergonomía avanzada y presencia ejecutiva.',
+        image:
+        '../public/img/categorias/directivos.png',
+        link: '#directivos'
+    },
+    {
+        title: 'Ejecutivos',
+        description:
+        'Sillas ergonómicas ideales para largas jornadas de trabajo, combinando confort, diseño y funcionalidad.',
+        image:
+        '../public/img/categorias/ejecutivos.png',
+        link: '#ejecutivos'
+    },
+    {
+        title: 'Operativos',
+        description:
+        'Soluciones prácticas y resistentes para áreas operativas, pensadas para el uso continuo y eficiente.',
+        image:
+        '../public/img/categorias/operativos.png',
+        link: '#operativos'
+    },
+    {
+        title: 'Industrial',
+        description:
+        'Sillas robustas diseñadas para entornos industriales, con alta durabilidad y fácil mantenimiento.',
+        image:
+        '../public/img/categorias/industrial.png',
+        link: '#industrial'
+    },
+    {
+        title: 'Accesorios',
+        description:
+        'Complementos y accesorios para sillas que mejoran la ergonomía, el confort y la personalización.',
+        image:
+        '../public/img/categorias/accesorios.png',
+        link: '#accesorios'
+    },
+    {
+        title: 'Visitantes Exterior',
+        description:
+        'Sillas para áreas exteriores, fabricadas con materiales resistentes a la intemperie y de fácil limpieza.',
+        image:
+        '../public/img/categorias/visitantes-exterior.png',
+        link: '#visitantes-exterior'
+    },
+    {
+        title: 'Visitantes Interior',
+        description:
+        'Sillas funcionales y estéticas para salas de espera, recepción y espacios de atención al cliente.',
+        image:
+        '../public/img/categorias/visitantes-interior.png',
+        link: '#visitantes-interior'
+    },
+    {
+        title: 'Mesas',
+        description:
+        'Mesas complementarias para oficinas, salas de juntas y áreas de trabajo colaborativo.',
+        image:
+        '../public/img/categorias/mesas.png',
+        link: '#mesas'
+    },
+    {
+        title: 'Confortables',
+        description:
+        'Sillas diseñadas para brindar máximo confort, ideales para descanso, espera prolongada o uso casual.',
+        image:
+        '../public/img/categorias/confortables.png',
+        link: '#confortables'
+    },
+    {
+        title: 'Bancas',
+        description:
+        'Bancas resistentes y funcionales para áreas comunes, instituciones y espacios de alto tránsito.',
+        image:
+        '../public/img/categorias/bancas.png',
+        link: '#bancas'
+    },
+    {
+        title: 'Escolar',
+        description:
+        'Mobiliario escolar ergonómico y duradero, diseñado para aulas, laboratorios y espacios educativos.',
+        image:
+        '../public/img/categorias/escolares.png',
+        link: '#escolar'
+    }
+    ];
